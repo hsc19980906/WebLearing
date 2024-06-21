68e319ad349857b004e8d10a5123d7fc15f7058a
double-star-admin <2289042370@qq.com>
2024-06-21 19:54:14 +0800

    feat:增加学习Razor页面的项目RazorPagesBook

---
+++ 03-AspDotCore/RazorPagesBook/Migrations/20240621072237_Type.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RazorPagesBook.Migrations
+{
+    public partial class Type : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "Book",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Book");
+        }
+    }
+}
---
+++ 03-AspDotCore/RazorPagesBook/Pages/Books/Index.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesBook.Data;
+using RazorPagesBook.Models;
+
+namespace RazorPagesBook.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesBook.Data.RazorPagesBookContext _context;
+
+        public IndexModel(RazorPagesBook.Data.RazorPagesBookContext context)
+        {
+            _context = context;
+        }
+        [BindProperty(SupportsGet = true)]
+        public string SearchKey { get; set; }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            var books=_context.Book.AsQueryable();//Linq查询from b in _context.Book select b
+            if(!string.IsNullOrEmpty(SearchKey) )
+            {
+                books = books.Where(b => b.Title.Contains(SearchKey));
+            }
+
+            Book = await books.ToListAsync();
+        }
+    }
+}
