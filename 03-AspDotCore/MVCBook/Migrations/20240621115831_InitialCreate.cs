c3873669bccab7c6c921754afe32107c66ffed0b
double-star-admin <2289042370@qq.com>
2024-06-21 20:14:29 +0800

    feat:增加MVC相关学习项目

---
+++ 03-AspDotCore/MVCBook/Migrations/20240621115831_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVCBook.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    PublicationDate = table.Column<DateTime>(nullable: false),
+                    Author = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(nullable: false),
+                    Type = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Book");
+        }
+    }
+}
---
+++ 03-AspDotCore/MVCBook/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace MVCBook.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        [Display(Name = "标题")]
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]//仅显示日期，而非时间信息
+        [Display(Name = "出版日期")]
+        public DateTime PublicationDate { get; set; }
+        [Display(Name = "作者")]
+        public string Author { get; set; }
+        [Display(Name = "价格")]
+        public decimal Price { get; set; }
+        [Display(Name = "类型")]
+        public string Type { get; set; }
+    }
+}
